e985135cb771c3b102ddcb988d9b9f8f544761ed
denghe <denghe@hotmail.com>
2016-02-29 20:07:46 +0800

    testing

--- Tests/vs_cs/vs_cs/Program.cs
+++ Tests/vs_cs/vs_cs/Program.cs
@@ -13,6 +13,17 @@ namespace vs_cs
         class Fooo { }
         static void Main( string[] args )
         {
+            var bb = new ByteBuffer();
+            var bs = new List<bool>();
+            for( int i = 0; i < 32; ++i )
+            {
+                bs.Add( ( i & 1 ) > 0 );
+            }
+            bb.Write( bs );
+            bb.Dump();
+            return;
+
+
             //var bb = new ByteBuffer();
             //{
             //    double d = 0;
@@ -41,51 +52,51 @@ namespace vs_cs
             //while( false );
 
 
-            var bb = new ByteBuffer();
-            var s = Stopwatch.StartNew();
-            for( int i = 0; i < 9; ++i )
-            {
-                var o = new CS.RootPkg1();
-                o.m_root_e1 = CS.Enum1.Item3;
-                o.m_ns1_pkg2.ns1_pkg1.ns1_enum1 = CS.Ns1.Enum1.item1;
-                o.m_ns1_pkg2.re1 = CS.Enum1.Item3;
-                o.m_byte = 12;
-                o.m_ushort = 12345;
-                o.m_uint = 12345678;
-                o.m_ulong = 1234567890123L;
-                o.m_sbyte = -12;
-                o.m_short = -12345;
-                o.m_int = -12345678;
-                o.m_long = -1234567890123L;
-                o.m_double = 1.234567890123;
-                o.m_float = 1.2345f;
-                o.m_bool = true;
-                o.m_DateTime = new DateTime( 2015, 5, 5, 0, 0, 0 );
-                o.m_string = "123456";
-                o.m_ByteBuffer.Write( (byte)1 );
-                o.m_ByteBuffer.Write( (byte)2 );
-                o.m_ByteBuffer.Write( (byte)3 );
-                o.m_bytes[ 0 ] = 0;
-                o.m_bytes[ 1 ] = 1;
-                o.m_bytes[ 6 ] = 7;
-                o.m_bytes[ 7 ] = 7;
-                o.m_ns2enum1s.Add( CS.Ns2.Enum1.item1 );
-                o.m_ns2enum1s.Add( CS.Ns2.Enum1.item1 );
-                o.m_ns2enum1s.Add( CS.Ns2.Enum1.item1 );
-                for( int j = 0; j < 32; ++j )
-                {
-                    o.m_bools.Add( j % 2 == 0 );
-                }
+            //var bb = new ByteBuffer();
+            //var s = Stopwatch.StartNew();
+            //for( int i = 0; i < 9; ++i )
+            //{
+            //    var o = new CS.RootPkg1();
+            //    o.m_root_e1 = CS.Enum1.Item3;
+            //    o.m_ns1_pkg2.ns1_pkg1.ns1_enum1 = CS.Ns1.Enum1.item1;
+            //    o.m_ns1_pkg2.re1 = CS.Enum1.Item3;
+            //    o.m_byte = 12;
+            //    o.m_ushort = 12345;
+            //    o.m_uint = 12345678;
+            //    o.m_ulong = 1234567890123L;
+            //    o.m_sbyte = -12;
+            //    o.m_short = -12345;
+            //    o.m_int = -12345678;
+            //    o.m_long = -1234567890123L;
+            //    o.m_double = 1.234567890123;
+            //    o.m_float = 1.2345f;
+            //    o.m_bool = true;
+            //    o.m_DateTime = new DateTime( 2015, 5, 5, 0, 0, 0 );
+            //    o.m_string = "123456";
+            //    o.m_ByteBuffer.Write( (byte)1 );
+            //    o.m_ByteBuffer.Write( (byte)2 );
+            //    o.m_ByteBuffer.Write( (byte)3 );
+            //    o.m_bytes[ 0 ] = 0;
+            //    o.m_bytes[ 1 ] = 1;
+            //    o.m_bytes[ 6 ] = 7;
+            //    o.m_bytes[ 7 ] = 7;
+            //    o.m_ns2enum1s.Add( CS.Ns2.Enum1.item1 );
+            //    o.m_ns2enum1s.Add( CS.Ns2.Enum1.item1 );
+            //    o.m_ns2enum1s.Add( CS.Ns2.Enum1.item1 );
+            //    for( int j = 0; j < 32; ++j )
+            //    {
+            //        o.m_bools.Add( j % 2 == 0 );
+            //    }
 
-                bb.Clear();
-                bb.Write( o );
-            }
-            Console.WriteLine( s.ElapsedMilliseconds );
-            bb.Dump();
-            {
-                var o = new CS.RootPkg1();
-                bb.Read( ref o );
-            }
+            //    bb.Clear();
+            //    bb.Write( o );
+            //}
+            //Console.WriteLine( s.ElapsedMilliseconds );
+            //bb.Dump();
+            //{
+            //    var o = new CS.RootPkg1();
+            //    bb.Read( ref o );
+            //}
         }
     }
 }
